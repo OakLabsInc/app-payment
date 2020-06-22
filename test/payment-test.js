2931ee5c29bb137f32f0a0ecdd5ad4912a7505ac
Richard Joffray <rjoffray@richards-mbp.lan>
2020-06-22 16:41:19 -0700

    added to the test script

--- test/payment-test.js
+++ test/payment-test.js
@@ -1,9 +1,9 @@
 const { exec } = require('child_process')
-
-let data = '{"cart":{"total":"40.4","tax":"3.434","taxRate":"0.085","grandTotal":"43.833999999999996"},"terminalIp":"192.168.86.43"}'
+console.log(process.env)
+let data = `{"cart":{"total":"40.4","tax":"3.434","taxRate":"0.085","grandTotal":"43.833999999999996"},"terminalIp":"${process.env.TERMINAL_IP}"}`
 
 async function sendCart(payload) {
-	await exec(`curl -X POST -H "Content-Type: application/json" -d ${payload} localhost:8003` (err, stdout, stderr) => {
+	await exec(`curl --header "Content-Type: application/json" --request POST --data '${data}' localhost:8003` (err, stdout, stderr) => {
     if (err) {
       // node couldn't execute the command
       console.log(`Error: ${err}`);
